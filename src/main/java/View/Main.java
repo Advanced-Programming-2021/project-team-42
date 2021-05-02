9438ecf124966bf441606229af3912ddac3ad6eb
HosseinUni7ed <themmdhossein@gmail.com>
2021-05-02 18:55:45 +0430

    Main Menu Added

--- src/main/java/View/Main.java
+++ src/main/java/View/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         RegisterMenu registerMenu = new RegisterMenu(null);
+        MainMenu mainMenu = new MainMenu(registerMenu);
         Menu.setScanner(new Scanner(System.in));
         registerMenu.run();
     }
