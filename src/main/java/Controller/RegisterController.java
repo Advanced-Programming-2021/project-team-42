e3687dfe1cd2a67f3b64aa44af55f3b06fd5e4b1
HosseinUni7ed <themmdhossein@gmail.com>
2021-05-02 13:00:51 +0430

    Signup Changes

--- src/main/java/Controller/RegisterController.java
+++ src/main/java/Controller/RegisterController.java
@@ -3,6 +3,7 @@ package Controller;
 import Model.User;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,27 +12,35 @@ public class RegisterController {
     private static final String FILE_PATH = "C:\\Users\\Vision\\IdeaProjects\\Game First Phase\\src\\main\\java\\Database";
     private static RegisterController instance = null;
 
-    private RegisterController(){}
+    private RegisterController() {
+    }
 
-    public void createNewUser(String username, String nickname, String password){
-        try{
-            User user = new User(username, password, nickname);
-            Gson gson = new GsonBuilder().create();
-            FileWriter fileWriter = new FileWriter(FILE_PATH + "\\" + user.getUsername() + ".json");
-            gson.toJson(user, fileWriter);
-            fileWriter.close();
-            System.out.println("User created successfully");
+    public void createNewUser(String username, String nickname, String password) {
+        if (UserController.getInstance().isUserWithThisFieldExists(username , 0))
+            System.out.println("User with username " + username + " already exists");
+        else {
+            if (UserController.getInstance().isUserWithThisFieldExists(nickname, 1))
+                System.out.println("User with nickname " + nickname + " already exists");
+            else {
+                try {
+                    User user = new User(username, password, nickname);
+                    Gson gson = new GsonBuilder().create();
+                    FileWriter fileWriter = new FileWriter(FILE_PATH + "\\" + user.getUsername() + ".json");
+                    gson.toJson(user, fileWriter);
+                    fileWriter.close();
+                    System.out.println("User created successfully");
 
-        }
-        catch (IOException e){
-            System.out.println("Could not create user\n" +
-                    "Please try again");
+                } catch (IOException e) {
+                    System.out.println("Could not create user\n" +
+                            "Please try again");
+                }
+            }
         }
     }
 
 
-    public static RegisterController getInstance(){
-        if(instance == null)
+    public static RegisterController getInstance() {
+        if (instance == null)
             instance = new RegisterController();
         return instance;
     }
--- src/main/java/Controller/UserController.java
+++ src/main/java/Controller/UserController.java
@@ -1,26 +1,66 @@
 package Controller;
 
+import Model.User;
+import com.google.gson.Gson;
+
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class UserController {
     private static UserController instance = null;
-    private UserController(){}
+    private static final String FILE_PATH = "C:\\Users\\Vision\\IdeaProjects\\Game First Phase\\src\\main\\java\\Database";
 
-    public boolean isUserExists(String username) {
-        File allUsers = new File("C:\\Users\\Vision\\IdeaProjects\\Game First Phase\\src\\main\\java\\Database");
-        File[] allUsersArray = allUsers.listFiles();
-        if (allUsersArray != null) {
-            for (File file : allUsersArray) {
-                if (file.getName().equals(username))
-                    return true;
+    private UserController() {
+    }
+
+//    public boolean isUserExists(String username) {
+//        Gson gson = new Gson();
+//        try {
+//            File allUsers = new File(FILE_PATH);
+//            File[] allUsersArray = allUsers.listFiles();
+//            if (allUsersArray != null) {
+//                for (File file : allUsersArray) {
+//                    FileReader fileReader = new FileReader(FILE_PATH + "\\" + file.getName());
+//                    User user = gson.fromJson(fileReader, User.class);
+//                    if(user.getNickname().equals(username))
+//                        return true;
+//                }
+//            }
+//        }catch(IOException e){
+//            System.out.println("Could not read information from file");
+//        }
+//        return false;
+//    }
+
+    public boolean isUserWithThisFieldExists(String field, int flag) {
+        Gson gson = new Gson();
+        try {
+            File allUsers = new File(FILE_PATH);
+            File[] allUsersArray = allUsers.listFiles();
+            if (allUsersArray != null) {
+                for (File file : allUsersArray) {
+                    FileReader fileReader = new FileReader(FILE_PATH + "\\" + file.getName());
+                    User user = gson.fromJson(fileReader, User.class);
+                    if(flag == 1) {
+                        if (user.getNickname().equals(field))
+                            return true;
+                    }
+                    else{
+                        if (user.getUsername().equals(field))
+                            return true;
+                    }
+                }
             }
+        }catch(IOException e){
+            System.out.println("Could not read information from file");
         }
         return false;
     }
 
 
     public static UserController getInstance() {
-        if(instance == null)
+        if (instance == null)
             instance = new UserController();
         return instance;
     }
