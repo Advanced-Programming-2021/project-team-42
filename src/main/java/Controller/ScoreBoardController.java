d499da2fbd8a6c8649d2601fe0cde7f7068e8dff
HosseinUni7ed <themmdhossein@gmail.com>
2021-05-04 20:25:26 +0430

    Profile Menu Added

--- src/main/java/Controller/ScoreBoardController.java
+++ src/main/java/Controller/ScoreBoardController.java
@@ -1,32 +1,5 @@
 package Controller;
 
-import Model.User;
-
-import java.util.ArrayList;
-import java.util.Comparator;
-
 public class ScoreBoardController {
-    private static ScoreBoardController instance = null;
 
-    private ScoreBoardController(){}
-
-    public void showScoreboard(){
-        int rank = 1;
-        ArrayList<User> allUsers = User.getAllUsers();
-        Comparator<User> sortUsers = Comparator.comparing(User::getScore , Comparator.reverseOrder()).
-                thenComparing(User::getNickname);
-        allUsers.sort(sortUsers);
-        for(User user : allUsers) {
-            System.out.println(rank + "- " + user.getNickname() + ": " + user.getScore());
-            rank++;
-        }
-        System.out.println("\n");
-    }
-
-
-    public static ScoreBoardController getInstance(){
-        if(instance == null)
-            instance = new ScoreBoardController();
-        return instance;
-    }
 }
